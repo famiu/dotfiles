c0019d00238d305b5f3ebbb6e3923ff31b48ca8f
Famiu Haque <famiuhaque@proton.me>
2024-10-20 15:06:41 +0600

    Let there be light

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if type -q direnv
+    direnv hook fish | source
+end
+
+if status is-interactive
+    function dotfiles --wraps '/usr/bin/env git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+        /usr/bin/env git --work-tree=$HOME/Dev/dotfiles/ $argv
+    end
+
+    function mkcd
+        mkdir -p $argv[1]; and cd $argv[1]
+    end
+
+    function nv
+        nvim $argv
+    end
+end
