f81cfa66fbdd457e6a0a18404864451513ee10fb
Famiu Haque <famiuhaque@proton.me>
2024-10-20 15:06:41 +0600

    Let there be light

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    function dotfiles --wraps '/usr/bin/env git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+        /usr/bin/env git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
+    end
+    function ollamakill
+        ollama ps | tail -n+2 | awk '{print $1}' | xargs -r -n1 ollama stop
+    end
+end
