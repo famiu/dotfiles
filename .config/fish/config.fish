3f762de25b463c189286f8d323726d44609d91c9
Famiu Haque <famiuhaque@proton.me>
2024-12-08 09:38:48 +0600

    remove unused commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,42 +7,6 @@ if status is-interactive
         /usr/bin/env git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
     end
 
-    function ollamakill
-        ollama ps | tail -n+2 | awk '{print $1}' | xargs -r -n1 ollama stop
-    end
-
-    function update_nvim
-        echo "Updating Neovim..."
-
-        set pwd (pwd)
-        set tmpdir (mktemp -d); or return 1
-        echo "Using temporary directory $tmpdir"
-
-        function cleanup -a tmpdir -a pwd
-            cd $pwd; or return 1
-            sudo rm -rf $tmpdir; or return 1
-            trap - SIGINT SIGTERM SIGQUIT; or return 1
-        end
-
-        function handle_abort -a tmpdir -a pwd
-            echo "Aborted. Cleaning up..."
-            cleanup $tmpdir $pwd; or return 1
-        end
-
-        sudo -v; or return 1
-        cd $tmpdir; or return 1
-
-        trap "handle_abort $tmpdir $pwd" SIGINT SIGTERM SIGQUIT
-
-        git clone https://github.com/neovim/neovim --depth 1; or handle_abort $tmpdir $pwd
-        cd neovim; or handle_abort $tmpdir $pwd
-        make CMAKE_BUILD_TYPE=RelWithDebInfo; or handle_abort $tmpdir $pwd
-        sudo make install; or handle_abort $tmpdir $pwd
-
-        echo "Installation complete"
-        cleanup $tmpdir $pwd
-    end
-
     function mkcd
         mkdir -p $argv[1]; and cd $argv[1]
     end
